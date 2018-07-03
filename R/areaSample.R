62521059dab5910bb5d50ab8500bfa1a87b07ab5
jacobstrunk001 <12001003134353@fedidcard.gov>
2018-07-03 13:57:19 -0700

    first commit

---
+++ R/areaSample.R
@@ -0,0 +1,78 @@
+#'@title
+#'  <Delete and Replace>
+#'
+#'@description
+#'  <Delete and Replace>
+#'
+#'@details
+#'  <Delete and Replace>
+#'
+#'\cr
+#'Revision History
+#' \tabular{ll}{
+#'1.0 \tab date and revisions.. \cr
+#'}
+#'
+#'@author
+#'
+#'Jacob Strunk <Jstrunk@@fs.fed.us>
+#'
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'@param
+#'
+#'@return
+#'  <Delete and Replace>
+#'
+#'@examples
+#'  <Delete and Replace>
+#'
+#'@import some_package,some_package2
+#'
+#'@export
+#
+#'@seealso \code{\link{another_function}}\cr \code{\link{yet_another_function}}\cr
+
+#Desired upgrades to this function:
+#
+#
+
+# x = function(x){}
+
+#copy function arguments and use this code to format arguments
+##writeClipboard(paste(gsub("^[[:space:]]*[,]*","#'@param ",gsub("=.*"," ",readClipboard())),collapse="\n"))
+
+areaSample=function(poly,raster,n,type=("area_prop","sp")){
+
+
+
+}
+
+rasterSample=function(raster,n,type=("area_prop","sp")){
+
+
+
+}
+
+gridSample=function(){
+
+
+}
+
+
+}
